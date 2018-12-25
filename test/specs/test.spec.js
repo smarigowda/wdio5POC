b7864d7f55fe63ce33f5c80326882b0384440723
Santosh Marigowda <santosharakere@gmail.com>
2018-12-25 17:12:49 +0000

    debugging the test using vs code

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -1,10 +1,12 @@
 const assert = require('assert');
 
-describe('webdriver.io page', () => {
+describe('webdriver.io page', async () => {
     it('should have the right title', async () => {
         // await browser.url('https://webdriver.io');
         await browser.url('https://www.google.com/');
+        debugger
         const title = await browser.getTitle();
         // assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+        assert.equal(1,1);
     });
 });
