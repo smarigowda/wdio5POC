d35880cf0485dc03dcaa0326aa88a109651f859f
Santosh Marigowda <santosharakere@gmail.com>
2018-12-26 07:43:18 +0000

    one more test

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 describe('Google Home Page', () => {
+    let title;
     before(() => {
         assert(1,1);
     });
@@ -9,9 +10,12 @@ describe('Google Home Page', () => {
     });
     it('should have the right title', () => {
         browser.url('https://www.google.com');
-        const title = browser.getTitle();
+        title = browser.getTitle();
         // browser.debug();
         assert(title,'Google');
         // assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
+    });
+    it('should have the right title', () => {
+        assert(title,'Google');
     });
 });
