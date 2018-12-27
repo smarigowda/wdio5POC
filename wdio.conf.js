13c68c678382f9fee866bc6e3adfb29b7282c73c
Santosh Marigowda <santosharakere@gmail.com>
2018-12-27 16:29:55 +0000

    running on both chrome and firefox

--- wdio.conf.js
+++ wdio.conf.js
@@ -46,13 +46,18 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
-    capabilities: [{
+    capabilities: [
+    {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
         maxInstances: 5,
         //
         browserName: 'chrome'
+    },
+    {
+        browserName: 'firefox',
+        maxInstances: 1,
     }],
     //
     // ===================
@@ -111,8 +116,8 @@ exports.config = {
     // commands. Instead, they hook themselves up into the test process.
     services: ['selenium-standalone'],
 
-    debug: true,
-    execArgv: ['--inspect-brk=127.0.0.1:5859'],
+    // debug: true,
+    // execArgv: ['--inspect-brk=127.0.0.1:5859'],
     // execArgv: ['--inspect=127.0.0.1:5859'],
 
     //
